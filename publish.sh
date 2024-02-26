98016b0cb05b51ae7ccf9dae3397e357b92e4932
Tsachi Herman <24438559+tsachiherman@users.noreply.github.com>
2024-02-26 13:15:12 -0500

    update

--- publish.sh
+++ publish.sh
@@ -1,3 +1,9 @@
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+if [ ${BRANCH} != "main" ]; then
+	echo "publish only works from main branch"
+	exit 1
+fi
+
 git checkout gh-pages
 git merge main
 git push
